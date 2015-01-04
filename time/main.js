a1764192f30bc0e9c494f5a1c93c34528d13b7f1
Marcus Strandberg <marcus.strandberg@live.se>
2015-01-04 03:44:27 +0100

    added date. cleaned up code

---
+++ time/main.js
@@ -0,0 +1,44 @@
+/* global $, window, document */
+
+(function () {
+  'use strict';
+  
+  function updateClock () {
+    var now = new Date(),
+        hour = now.getHours(),
+        minute = now.getMinutes(),
+        second = now.getSeconds();
+    
+    rotateArm('hour', hour);
+    rotateArm('minute', minute);
+    rotateArm('second', second);
+    
+    document.title = now.getTime();
+    
+    return now;
+  }
+  
+  var days = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'],
+      months = ['jan', 'febr', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec'];
+  
+  Date.prototype.getMonthName = function () {
+    return months[this.getMonth()];
+  };
+  
+  Date.prototype.getDayName = function () {
+    return days[this.getDay()];
+  };
+  
+  function rotateArm(arm, time) {
+    $('.' + arm).css('-webkit-transform', 'rotate(' + ((arm === 'hour') ? time * 15 : time * 6) + 'deg)');
+  }
+  
+  $('document').ready(function () {
+    var currentTime = updateClock();
+    
+    $('.date p').text(currentTime.getDayName() + ' | ' + currentTime.getMonthName() + ' ' + currentTime.getDate());
+    
+    window.setInterval(updateClock, 1000);
+  });
+  
+}());
