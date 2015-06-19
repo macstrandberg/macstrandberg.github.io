0c964576e159f652a2e1d213f7c61f7d56231a56
Marcus Strandberg <marcus.strandberg@live.se>
2015-06-19 16:43:57 +0200

    split styles into separate stylesheets

--- time/main.js
+++ time/main.js
@@ -1,27 +1,29 @@
-!function () {
+(function () {
   'use strict';
 
+  /* todo:
+   * re-write to use animation frames(correct term?)
+   * fix hour-hand to properly align closer to next hour when minutes > 30
+   * make second-hand seamlessly float between seconds, instead of jumping to the next second each second
+  **/
+
   var currentTime,
-      now,
-      hour,
-      minute,
-      second,
-      clockFace = document.getElementsByClassName('clock-face')[0],
-      armMiddle = document.getElementsByClassName('arm middle')[0],
-      armHour = document.getElementsByClassName('arm hour')[0],
-      armMinute = document.getElementsByClassName('arm minute')[0],
-      armSecond = document.getElementsByClassName('arm second')[0],
       dateElement = document.querySelectorAll('.date p')[0],
-      lightBackground = '#E3F2FD',
-      lightHour = '#2196F3',
-      lightMinute = '#82B1FF',
-      lightDate = 'rgba(0, 0, 0, 0.87)',
-      darkBackground = '#263238',
-      darkMinute = '#607D8B',
-      darkSecond = '#90AfAE',
-      darkDate = 'rgba(255, 255, 255, 1)',
       days = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'],
-      months = ['jan', 'febr', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec'];
+      head = document.getElementsByTagName('head')[0],
+      hour,
+      hourArm = document.getElementsByClassName('hour')[0],
+      millisecond,
+      minute,
+      minuteArm = document.getElementsByClassName('minute')[0],
+      months = ['jan', 'febr', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec'],
+      now,
+      second,
+      secondArm = document.getElementsByClassName('second')[0],
+      stylesheet = document.createElement('link');
+
+  stylesheet.rel = 'stylesheet';
+  stylesheet.type = 'text/css';
 
   Date.prototype.getMonthName = function () {
     return months[this.getMonth()];
@@ -31,43 +33,43 @@
     return days[this.getDay()];
   };
 
+  function changeStyleByHour (hour) {
+    if (hour > 6 && hour < 22) {
+      stylesheet.href = 'bright.css';
+    } else {
+      stylesheet.href = 'dark.css';
+    }
+
+    head.appendChild(stylesheet);
+  }
+
   function updateClock () {
-    now = new Date(),
-    hour = now.getHours(),
-    minute = now.getMinutes(),
+    now = new Date();
+    hour = now.getHours();
+    minute = now.getMinutes();
     second = now.getSeconds();
+    millisecond = now.getMilliseconds();
 
     rotateArm('hour', hour);
     rotateArm('minute', minute);
     rotateArm('second', second);
 
-    window.changeStyleByHour(hour);
+    changeStyleByHour(hour);
 
     document.title = now.getTime();
 
     return now;
   }
 
-  window.changeStyleByHour = function (hour) {
-    if (hour > 6 && hour < 22) {
-      clockFace.style.backgroundColor = lightBackground;
-      armMiddle.style.backgroundColor = lightBackground;
-      armHour.style.backgroundColor = lightMinute;
-      armMinute.style.backgroundColor = lightMinute;
-      armSecond.style.backgroundColor = lightHour;
-      dateElement.style.color = lightDate;
-    } else {
-      clockFace.style.backgroundColor = darkBackground;
-      armMiddle.style.backgroundColor = darkBackground;
-      armHour.style.backgroundColor = darkSecond;
-      armMinute.style.backgroundColor = darkSecond;
-      armSecond.style.backgroundColor = darkMinute;
-      dateElement.style.color = darkDate;
-    }
-  }
-
   function rotateArm(arm, time) {
-    document.getElementsByClassName(arm)[0].style.transform = 'rotate(' + ((arm === 'hour') ? (time % 12) * 30 : time * 6) + 'deg)';
+    if (arm === 'hour') {
+      hourArm.style.transform = 'rotate(' + (time % 12) * 30 + 'deg)';
+    } else if (arm === 'minute') {
+      minuteArm.style.transform = 'rotate(' + time * 6 + 'deg)';
+    } else {
+      secondArm.style.transform = 'rotate(' + time * 6 + 'deg)';
+    }
+    // document.getElementsByClassName(arm)[0].style.transform = 'rotate(' + ((arm === 'hour') ? (time % 12) * 30 : time * 6) + 'deg)';
   }
 
   document.addEventListener('DOMContentLoaded', function () {
@@ -78,4 +80,4 @@
     window.setInterval(updateClock, 1000);
   });
 
-}();
+}());
