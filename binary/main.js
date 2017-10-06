b6c2a0f6d3149be1a4647621345e80159d968187
mhrvatin <marcus@hrvatin.se>
2017-10-06 19:35:16 +0200

    Renamed projects and added crude binary clock

---
+++ binary/main.js
@@ -0,0 +1,166 @@
+(function () {
+  'use strict';
+
+  let now = moment().format('HH:mm:ss'),
+    $clockFace = document.querySelectorAll('.clock-face')[0];
+
+  window.time = {
+    hour: 0,
+    minute: 0,
+    second: 0,
+    setHour: function(hour) {
+      this.hour = hour;
+    },
+    setMinute: function(minute) {
+      this.minute = minute;
+    },
+    setSecond: function(second) {
+      this.second = second;
+    },
+    getHour: function() {
+      return this.hour;
+    },
+    getMinute: function() {
+      return this.minute;
+    },
+    getSecond: function() {
+      return this.second;
+    },
+    setColor: function(timeArr, type) {
+    $clockFace[0].querySelectorAll(table + ' ' + number + ' ' + cell)[0]
+        .style.backgroundColor = color;
+    },
+    pad: function(n, width, z) {
+      z = z || '0';
+      n = n + '';
+      return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
+    },
+    calculateBinaryTime: function () {
+      let Debug = false;
+      let decTimes = [];
+      let binTimes = [];
+      let columnArrangedBinTimes = [];
+      let rowArrangedBinTimes = [[],[],[],[]];
+
+      decTimes.push(this.getHour() + '');
+      decTimes.push(this.getMinute() + '');
+      decTimes.push(this.getSecond() + '');
+
+      for (let i = 0; i < 3; i++) {
+        binTimes.push(decTimes[i]); // push decimal time to array
+        Debug ? console.debug('****************************') : null;
+        Debug ? console.debug('pushed decimal') : null;
+        Debug ? console.debug(binTimes) : null;
+
+        // push first digit
+        binTimes.push(decTimes[i].substring(0, 1));
+        Debug ? console.debug('pushed first digit') : null;
+        Debug ? console.debug(binTimes) : null;
+        // convert first digit to bin
+        binTimes[i + 1] = (binTimes[i + 1] >>> 0).toString(2);
+        Debug ? console.debug('converted first digit to bin') : null;
+        Debug ? console.debug(binTimes) : null;
+
+        if (decTimes[i].length === 2) {
+          // adding splitter
+          binTimes[i + 1] += '-';
+          Debug ? console.debug('splitter added') : null;
+          Debug ? console.debug(binTimes) : null;
+
+          // push second digit
+          binTimes.push(decTimes[i].substring(1));
+          Debug ? console.debug('pushed second digit to bin') : null;
+          Debug ? console.debug(binTimes) : null;
+          // convert second digit to bin
+          binTimes[i + 2] = (binTimes[i + 2] >>> 0).toString(2);
+          Debug ? console.debug('converted second digit to bin') : null;
+          Debug ? console.debug(binTimes) : null;
+        }
+
+        // remove first and last element(decimal time, );
+        binTimes.splice(i, 1);
+        Debug ? console.debug('spliced i,1') : null;
+        Debug ? console.debug(binTimes) : null;
+
+        if (decTimes[i].length === 2) {
+          // adding first and second digits to one element
+          binTimes[i] += binTimes[i + 1];
+          Debug ? console.debug('concat') : null;
+          Debug ? console.debug(binTimes) : null;
+
+          // removing last element
+          binTimes.splice(-1, 1);
+          Debug ? console.debug('spliced -1,1') : null;
+          Debug ? console.debug(binTimes) : null;
+        }
+        Debug ? console.debug('****************************') : null;
+
+        // document.getElementById('binTimeString').innerText = binTimes;
+
+        // push it to the columnArrangedBinTimesurn array
+        columnArrangedBinTimes.push(binTimes[i]);
+      }
+
+      for (let i = 0; i < columnArrangedBinTimes.length; i++) { // remove dash
+        columnArrangedBinTimes[i] = columnArrangedBinTimes[i].split('-');
+      }
+
+      for (let i = 0; i < columnArrangedBinTimes.length; i++) { // left zero padding time-strings
+        for (let j = 0; j < 2; j++) {
+          columnArrangedBinTimes[i][j] = this.pad(columnArrangedBinTimes[i][j], 4);
+        }
+      }
+
+      for (let i = 0; i < 3; i++) { // convert from line to column
+        for (let j = 0; j < 4; j++) {
+          for (let k = 0; k < 2; k++) {
+            rowArrangedBinTimes[j] += columnArrangedBinTimes[i][k].substr(j, 1);
+          }
+        }
+      }
+
+      return rowArrangedBinTimes;
+    },
+    parseBinaryTime: function (timeArr) {
+      for (let i = 0; i < 4; i++) { // row
+        if (timeArr[i] !== undefined) {
+          for (let j = 0; j < 6; j++) { // col/cell
+            if (timeArr[i][j] === "1") {
+              document.querySelectorAll('tbody > tr')[i].querySelectorAll('td')[j].textContent = '*';
+            } else {
+              document.querySelectorAll('tbody > tr')[i].querySelectorAll('td')[j].innerHTML = '&nbsp;';
+            }
+          }
+        }
+      }
+    },
+    draw: function(hour, minute, second) {
+      this.setHour(hour);
+      this.setMinute(minute);
+      this.setSecond(second);
+
+      window.timeArr = this.calculateBinaryTime();
+      this.parseBinaryTime(timeArr);
+
+      // this.setColor(this.calculateBinaryTime());
+    }
+  };
+
+  function update() {
+    let now = moment();
+    window.time.draw(now.hour(), now.minute(), now.second());
+    requestAnimationFrame(update);
+  }
+
+  document.addEventListener('DOMContentLoaded', function () {
+
+    requestAnimationFrame(update);
+
+    // console.group('Time object');
+    //   console.debug('hour %i', window.time.getHour());
+    //   console.debug('minute %i', window.time.getMinute());
+    //   console.debug('second %i', window.time.getSecond());
+    // console.groupEnd();
+  });
+
+}());
---
+++ digital/main.js
@@ -0,0 +1,141 @@
+(function () {
+  'use strict';
+
+  let days = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'],
+      dateElement = document.querySelectorAll('.date p')[0],
+      head = document.getElementsByTagName('head')[0],
+      months = ['jan', 'feb', 'mar', 'apr', 'may', 'jun',
+                'jul', 'aug', 'sep', 'oct', 'nov', 'dec'],
+      stylesheets = [
+        ["#FFF8E1", "#FFE57F", "#FFC107"], // amber
+        ["#E3F2FD", "#82B1FF", "#2196F3"], // blue
+        ["#E0F7FA", "#84FFFF", "#00BCD4"], // cyan
+        ["#FBE9E7", "#FF9E80", "#FF5722"], // deepOrange
+        ["#EDE7F6", "#B388FF", "#673AB7"], // deepPurple
+        ["#E8F5E9", "#B9F6CA", "#4CAF50"], // green
+        ["#E8EAF6", "#8C9EFF", "#3F51B5"], // indigo
+        ["#E1F5FE", "#80D8FF", "#03A9F4"], // lightBlue
+        ["#F1F8E9", "#CCFF90", "#8BC34A"], // lightGreen
+        ["#F9FBE7", "#F4FF81", "#CDDC39"], // lime
+        ["#FFF3E0", "#FFD180", "#FF9800"], // orange
+        ["#FCE4EC", "#FF80AB", "#E91E63"], // pink
+        ["#F3E5F5", "#EA80FC", "#9C27B0"], // purple
+        ["#FFEBEE", "#FF8A80", "#F44336"], // red
+        ["#E0F2F1", "#A7FFEB", "#009688"], // teal
+        ["#FFFDE7", "#FFFF8D", "#FFEB3B"], // yellow
+        ["#263238", "#90AFAE", "#607D8B"]  // night
+      ];
+
+  Date.prototype.getMonthName = function () {
+    return months[this.getMonth()];
+  };
+
+  Date.prototype.getDayName = function () {
+    return days[this.getDay()];
+  };
+
+  function setStyle() {
+    let style,
+        sheet = document.styleSheets[0],
+        rules = sheet.rules,
+        hour = new Date().getHours();
+
+    if (hour > 6 && hour < 22) {
+      style = Math.floor(Math.random() * 15); // random style
+    } else {
+      style = 16; // dark style
+      document.querySelector('.date').style.color = 'rgba(255, 255, 255, 1)';
+    }
+
+    document.querySelector('.clock').style.backgroundColor = stylesheets[style][0];
+    sheet.insertRule('.clock:before {background-color: ' + stylesheets[style][0] + ';}', rules.length);
+    document.querySelector('.hour').style.backgroundColor = stylesheets[style][1];
+    document.querySelector('.minute').style.backgroundColor = stylesheets[style][1];
+    document.querySelector('.second').style.backgroundColor = stylesheets[style][2];
+  }
+
+  function initLocalClocks() {
+    let date = new Date;
+    let seconds = date.getSeconds();
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+
+    let hands = [
+      {hand: 'hour', angle: (hours * 30) + (minutes / 2)},
+      {hand: 'minute', angle: (minutes * 6)},
+      {hand: 'second', angle: (seconds * 6)}
+    ];
+
+    // Loop through each of these hands to set their angle
+    for (let j = 0; j < hands.length; j++) {
+      let elements = document.querySelectorAll('.' + hands[j].hand);
+
+      for (let k = 0; k < elements.length; k++) {
+        elements[k].style.webkitTransform = 'rotateZ('+ hands[j].angle +'deg)';
+        elements[k].style.transform = 'rotateZ('+ hands[j].angle +'deg)';
+
+        // If this is a minute hand, note the seconds position (to calculate minute position later)
+        if (hands[j].hand === 'minute') {
+          elements[k].parentNode.setAttribute('data-second-angle', hands[j + 1].angle);
+        }
+      }
+    }
+  }
+
+  /*
+   * Set a timeout for the first minute hand movement (less than 1 minute), then rotate it every minute after that
+   */
+  function setUpMinuteHands() {
+    // Find out how far into the minute we are
+    let containers = document.querySelectorAll('.minute-container');
+    let secondAngle = containers[0].getAttribute("data-second-angle");
+
+    if (secondAngle > 0) {
+      // Set a timeout until the end of the current minute, to move the hand
+      let delay = (((360 - secondAngle) / 6) + 0.1) * 1000;
+
+      setTimeout(function() {
+        moveMinuteHands(containers);
+      }, delay);
+    }
+  }
+
+  /*
+   * Do the first minute's rotation
+   */
+  function moveMinuteHands(containers) {
+    for (let i = 0; i < containers.length; i++) {
+      containers[i].style.webkitTransform = 'rotateZ(6deg)';
+      containers[i].style.transform = 'rotateZ(6deg)';
+    }
+
+    // Then continue with a 60 second interval
+    setInterval(function() {
+      for (let i = 0; i < containers.length; i++) {
+        if (containers[i].angle === undefined) {
+          containers[i].angle = 12;
+        } else {
+          containers[i].angle += 6;
+        }
+        containers[i].style.webkitTransform = 'rotateZ('+ containers[i].angle +'deg)';
+        containers[i].style.transform = 'rotateZ('+ containers[i].angle +'deg)';
+      }
+    }, 60000);
+  }
+
+  function update() {
+    requestAnimationFrame(update);
+    document.title = new Date().getTime();
+  }
+
+  document.addEventListener('DOMContentLoaded', function () {
+    dateElement.textContent = new Date().getDayName() + ' | ' +
+    new Date().getMonthName() + ' ' + new Date().getDate();
+
+    initLocalClocks();
+    setUpMinuteHands();
+    setStyle();
+
+    requestAnimationFrame(update);
+  });
+}());
